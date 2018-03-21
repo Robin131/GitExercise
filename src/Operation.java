6d2ddf78af44d7b570204b19d95d93bdd6d744c4
Robin131 <824009273@qq.com>
2018-03-21 09:18:18 +0100

    ttt

---
+++ src/Operation.java
@@ -0,0 +1,32 @@
+
+public abstract class Operation {
+	protected double a;
+	protected double b;
+	
+	public Operation() {
+		super();
+	}
+	
+	public abstract String getName();
+	
+	public abstract String getOperator();
+	
+	public abstract double calc();
+	
+	public void setNumbers(double a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public static Operation parseOperator(String op) {
+		Operation[] ops = new Operation[] {new Addition(), new Multiplication()};
+		for(Operation o : ops) {
+			if(o.getOperator().equals(op)) {
+				return o;
+			}
+		}
+		return null;
+	}
+	
+	
+}
